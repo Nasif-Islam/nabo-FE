bc534655ef6cdf150fdf2c0d5c4c413a00adce73
Nasif Islam <178942734+Nasif-Islam@users.noreply.github.com>
2026-02-24 09:28:58 +0000

    Initilaise pages and component structure - set up routing in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import NavBar from "./components/Navbar";
+import ArticleList from "./components/ArticleList";
+import ArticleCard from "./components/ArticleCard";
+import ErrorPage from "./components/ErrorPage";
 
 function App() {
   return (
-    <>
-      <h1>Nabo</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<ArticleList />} />
+        <Route path="/articles/:article_id" element={<SingleArticle />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <NavBar />
+    </BrowserRouter>
   );
 }
 
