066b443f9fd2fc7b43288bf03e02be0ae5fc4bbd
Nasif Islam <178942734+Nasif-Islam@users.noreply.github.com>
2026-02-27 15:06:10 +0000

    featL add bottom navbar that directs to pages and css styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,24 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <nav></nav>;
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/post">Post</NavLink>
+        </li>
+        <li>
+          <NavLink to="/topics">Topics</NavLink>
+        </li>
+        <li>
+          <NavLink to="/settings">Settings</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
